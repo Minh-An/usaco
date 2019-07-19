208725111c6078d2263292ba30c9eb34086a47a6
An Doan <minh.an.doan@gmail.com>
2019-07-18 20:24:18 -0700

    Test union find algorithm

--- union-find/main.cpp
+++ union-find/main.cpp
@@ -5,24 +5,30 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    vector <int> pdata(2000000);
+    vector <int> qdata(2000000);
     ifstream ifs("large.txt", ifstream::in);
     int n;
     ifs >> n;
     
     UnionFind uf = UnionFind(n);
 
-    clock_t begin = clock();
-    while(ifs.eof() == false)
+    int i = 0;
+    while(!ifs.eof())
     {
         int p, q; 
-        ifs >> p;
-        ifs >> q;
-        if (uf.Connected(p,q)) { continue; }
-        uf.Union(p,q);
+        ifs >> pdata[i];
+        ifs >> qdata[i++];
+        
         //cout << p << " " << q << endl;
     }
+
+    clock_t begin = clock();
+    for(int i = 0; i < pdata.size(); i++) {
+        if (uf.Connected(pdata[i],qdata[i])) { continue; }
+        uf.Union(pdata[i],qdata[i]);
+    }
     clock_t endt = clock();
-    cout << (endt-begin) * 1000 / CLOCKS_PER_SEC << "ms" << endl;
+    cout << (endt-begin) * 1000 / CLOCKS_PER_SEC << "ms" << endl; //283ms
     cout << uf.Count() << " components" << endl;
 }
